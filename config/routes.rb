96c0a547a411da87905ac151231a12798473bbc4
JaimeGaribaldo7 <jaimegaribaldo753@gmail.com>
2016-09-22 11:10:40 -0400

    Add get to patients/:id and added 2 patients

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
 
-  resources :patients, only: [:index]
+  resources :patients, only: [:index, :show]
   # same as^^^^
   # get '/patients', to: 'patients#index'
 end
